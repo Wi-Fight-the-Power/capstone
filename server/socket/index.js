49e875675a806761cd6d76d3949cdcfb7690c7cf
williamg1750 <william.guan12@gmail.com>
2021-03-23 15:21:23 -0400

    saving before pulling

--- server/socket/index.js
+++ server/socket/index.js
@@ -2,11 +2,21 @@ module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
-    socket.on('message', message => {
-      socket.broadcast.emit('message', message)
+    // socket.on('message', message => {
+    //   socket.broadcast.emit('message', message)
+    // })
+
+    // socket.on('drawing', data => socket.broadcast.emit('drawing', data))
+
+    socket.on('Join Room', room => {socket.join(room)})
+
+    socket.on('message', ({room , message}) => {
+      socket.to(room).emit("message", message)
     })
 
-    socket.on('drawing', data => socket.broadcast.emit('drawing', data))
+    socket.on('drawing', ({room , data}) => {
+      socket.to(room).emit("data", data)
+    })
 
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
