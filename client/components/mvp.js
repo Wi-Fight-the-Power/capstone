d1390b0a79faeb8b7e517696a29c80b1beb1dc18
Shawn Gay <Shawn.Gay.Jr@gmail.com>
2021-03-25 09:45:49 -0500

    DrawAndView

--- client/components/mvp.js
+++ client/components/mvp.js
@@ -1,21 +1,59 @@
 import React from 'react'
 import Chatbox from './chatbox'
-import Board from './whiteboardv1'
+import Board from './whiteboard'
 import socket from '../socket'
+import ViewBoard from './whiteBoardViewer'
 
 
-const Mvp = (props) => {
-  const roomNum = props.match.params.id
-  console.log('socket.rooms)',socket.rooms)
-  console.log('connected',roomNum)
-  socket.emit('Join Room', roomNum);
-  return (
-    <div className="drawinggame">
-      <h1>Room code:{roomNum}</h1>
-      <Board roomNum={roomNum}/>
-      <Chatbox roomNum={roomNum}/>
-    </div>
-  )
+class Mvp extends React.Component{
+  constructor(props){
+    super(props)
+    this.state= {
+      view: true,
+
+    }
+    this.changeView = this.changeView.bind(this)
+  }
+  componentDidMount(){
+    const roomNum = this.props.match.params.id
+    socket.emit('Join Room', roomNum);
+  }
+
+  componentWillUnmount(){
+    socket.emit('leaveRoom', )
+  }
+
+  changeView(){
+    const currentView = this.state.view
+    this.setState({view: !currentView})
+  }
+
+  render(){
+    const roomNum = this.props.match.params.id
+
+  if(this.state.view){
+    return (
+      <div className="drawinggame">
+        <h1>Room code:{roomNum}</h1>
+        <h1>Viewer</h1>
+        <ViewBoard roomNum={roomNum}/>
+        <Chatbox roomNum={roomNum}/>
+        <button type="submit" id="room num" onClick={() => {this.changeView()}}>View/Draw</button>
+      </div>
+    )
+  }
+  if(!this.state.view){
+    return (
+      <div className="drawinggame">
+        <h1>Room code:{roomNum}</h1>
+        <h1>Drawer</h1>
+        <Board roomNum={roomNum}/>
+        <Chatbox roomNum={roomNum}/>
+        <button type="submit" id="room num" onClick={() => {this.changeView()}}>View/Draw</button>
+      </div>
+    )
+  }
+  }
 }
 
 export default Mvp
