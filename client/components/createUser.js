d8b1346d19f004257bb76f1e9f2d90684dbf5509
Shawn Gay <Shawn.Gay.Jr@gmail.com>
2021-03-30 16:32:22 -0500

    Sockets now has username and room key values. Removed comments on createUser component

--- client/components/createUser.js
+++ client/components/createUser.js
@@ -1,71 +1,5 @@
-// import React from 'react'
-// import {connect} from 'react-redux'
-// import {sendUser, sendMe} from '../store/game'
-// import {Link} from 'react-router-dom';
-
-
-// class CreateUser extends React.Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       handle: '',
-//       isDrawing: false,
-//       score: 0
-//     }
-//     this.handleChange = this.handleChange.bind(this)
-//     this.handleSubmit = this.handleSubmit.bind(this)
-//   }
-
-//   handleChange(event) {
-//     this.setState({
-//       handle: event.target.value
-//     })
-//   }
-
-//   handleSubmit(event) {
-//     event.preventDefault()
-//     const user = JSON.stringify(this.state);
-//     localStorage.setItem('user', user);
-//     this.props.sendUser(this.state, this.props.roomNum)
-//     this.props.sendMe(this.state)
-//     this.setState({
-//       handle: '',
-//       isDrawing: false,
-//       score: 0,
-//       word: ''
-//     })
-//   }
-
-//   render() {
-//     const {handle} = this.state
-//     return (
-//       <div>
-//         <form onSubmit={this.handleSubmit}>
-//         <h2>
-//           <label htmlFor='username'>Create Username:</label>
-//         </h2>
-//           <input type='text' name='username' value={handle} onChange={this.handleChange}/>
-//         <button type="submit" >Create</button>
-//         </form>
-//         <Link to='/lobby'>Lobby</Link>
-//       </div>
-//     )
-//   }
-// }
-
-// const mapDispatch = dispatch => {
-//   return {
-//    sendUser: (user, room) => dispatch(sendUser(user, room)),
-//    sendMe: (me) => dispatch(sendMe(me)),
-//   }
-// }
-
-// export default connect(null, mapDispatch)(CreateUser)
-
-
-
-
 import React from 'react'
+import socket from '../socket'
 import {connect} from 'react-redux'
 import {sendUser, sendMe} from '../store/game'
 import Button from '@material-ui/core/Button';
@@ -105,6 +39,9 @@ class CreateUser extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault()
+    //Sending name to socket to create a Key:Value
+    socket.emit('userToSocket', this.state.handle, this.props.roomNum)
+
     const user = JSON.stringify(this.state);
     localStorage.setItem('user', user);
     this.props.sendUser(this.state, this.props.roomNum)
