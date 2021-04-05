204b9faf29e59770da4eda038264b99bec7c8564
Shawn Gay <Shawn.Gay.Jr@gmail.com>
2021-04-05 15:07:56 -0500

    Added 7 colors, colors now show in the selector tab, new icon in public folder, timer.js has 750ms wait to rotate again

--- client/components/whiteboard.js
+++ client/components/whiteboard.js
@@ -68,6 +68,10 @@ const useStyles = makeStyles({
    background: "#666666 !important",
    color: 'white'
  },
+ pen: {
+   background: "#666666 !important",
+   color: 'white'
+ },
  white: {
    background: 'white',
    '&:hover': {
@@ -110,7 +114,67 @@ const useStyles = makeStyles({
               background: '#9c9c9c !important',
               color: 'white !important'
            },
-  }
+    },
+    lightBlue: {
+      background: '#4deeea',
+      '&:hover': {
+        background: '#9c9c9c !important',
+        color: 'white !important'
+      },
+    },
+    lime: {
+      background: '#74ee15',
+      '&:hover': {
+        background: '#9c9c9c !important',
+        color: 'white !important'
+      },
+    },
+    neonPink: {
+      background: '#f000ff',
+      color: 'white',
+      '&:hover': {
+        background: '#9c9c9c !important',
+        color: 'white !important'
+      },
+    },
+    red: {
+      background: '#ff073a',
+      color: 'white',
+      '&:hover': {
+        background: '#9c9c9c !important',
+        color: 'white !important'
+      },
+    },
+    orange: {
+      background: '#FF5F1F',
+      '&:hover': {
+        background: '#9c9c9c !important',
+        color: 'white !important'
+      },
+    },
+   brown: {
+     background: '#c3732a',
+        '&:hover': {
+           background: '#9c9c9c !important',
+           color: 'white !important'
+           },
+   },
+   penColor: {
+     background: color,
+
+     color: color === 'black' ||
+     color === '#ff073a' ||
+     color === '#f000ff' ?
+     'white' : 'black'
+   },
+   bgColor: {
+     background: background,
+
+     color: background === 'black' ||
+     background === '#ff073a' ||
+     background === '#f000ff' ?
+     'white' : 'black'
+   }
 });
 
 const classes=useStyles()
@@ -124,9 +188,13 @@ const classes=useStyles()
 
 
         {/* change background */}
+
         <div className='easel'>
         <Select
         className="drawTools"
+        classes={{
+          root: classes.bgColor,
+        }}
         value={background}
         onChange={e => {
           sendBackground(e.target.value)
@@ -161,15 +229,49 @@ const classes=useStyles()
           root: classes.yellow,
           selected: classes.selected
         }} value="#FBE7C6">Yellow</MenuItem>
+
+        <MenuItem classes={{
+          root: classes.lightBlue,
+          selected: classes.selected
+        }} value="#4deeea">Light Blue</MenuItem>
+
+        <MenuItem classes={{
+          root: classes.lime,
+          selected: classes.selected
+        }} value='#74ee15'>Lime</MenuItem>
+
+        <MenuItem classes={{
+          root: classes.neonPink,
+          selected: classes.selected
+        }} value="#f000ff">Neon Pink</MenuItem>
+
+        <MenuItem classes={{
+          root: classes.red,
+          selected: classes.selected
+        }} value="#ff073a">Red</MenuItem>
+
+        <MenuItem classes={{
+          root: classes.orange,
+          selected: classes.selected
+        }} value="#FF5F1F">Orange</MenuItem>
+
+        <MenuItem classes={{
+          root: classes.brown,
+          selected: classes.selected
+        }} value="#c3732a">Brown</MenuItem>
         </Select>
 
 
         {/* change draw colors */}
         <Select
         className="drawTools"
+        classes={{
+            root: classes.penColor,
+            selected: classes.penColor
+        }}
         value={color}
         onChange={e => changeColor(e.target.value)}>
-        <MenuItem  classes={{
+         <MenuItem  classes={{
           root: classes.white,
           selected: classes.selected
         }} value="white">White</MenuItem>
@@ -198,6 +300,36 @@ const classes=useStyles()
           root: classes.yellow,
           selected: classes.selected
         }} value="#FBE7C6">Yellow</MenuItem>
+
+        <MenuItem classes={{
+          root: classes.lightBlue,
+          selected: classes.selected
+        }} value="#4deeea">Light Blue</MenuItem>
+
+        <MenuItem classes={{
+          root: classes.lime,
+          selected: classes.selected
+        }} value='#74ee15'>Lime</MenuItem>
+
+        <MenuItem classes={{
+          root: classes.neonPink,
+          selected: classes.selected
+        }} value="#f000ff">Neon Pink</MenuItem>
+
+        <MenuItem classes={{
+          root: classes.red,
+          selected: classes.selected
+        }} value="#ff073a">Red</MenuItem>
+
+        <MenuItem classes={{
+          root: classes.orange,
+          selected: classes.selected
+        }} value="#FF5F1F">Orange</MenuItem>
+
+        <MenuItem classes={{
+          root: classes.brown,
+          selected: classes.selected
+        }} value="#c3732a">Brown</MenuItem>
       </Select>
 
 
@@ -205,12 +337,19 @@ const classes=useStyles()
       <Select
         className="drawTools"
         value={tool}
+        classes= {{
+          root: classes.pen
+        }}
         onChange={e => {
           setTool(e.target.value)
         }}
       >
-        <MenuItem value="pen">Pen</MenuItem>
-        <MenuItem value="eraser">Eraser</MenuItem>
+        <MenuItem classes={{
+          selected: classes.selected
+        }} value="pen">Pen</MenuItem>
+        <MenuItem classes={{
+          selected: classes.selected
+        }} value="eraser">Eraser</MenuItem>
         {/* <option value="fill">Fill</option> */}
       </Select>
       </div>
